28a3debbeb4f842793eda392c89fcbe2bcc10859
Max Zhu <hello@zhuchenye.com>
2014-05-31 20:59:42 +0100

    add more scaladoc

--- src/main/scala/io/lamma/Lamma.scala
+++ src/main/scala/io/lamma/Lamma.scala
@@ -14,14 +14,23 @@ import io.lamma.Recurrence.EveryDay
 object Lamma {
 
   /**
+   * generate a full schedule based on input arguments <br>
+   * <br>
+   * For example: <br>
    *
-   * @param start
-   * @param end
-   * @param pattern
-   * @param startRule
-   * @param endRule
-   * @param dateDefs
-   * @return
+   * <pre>
+   * ||    Period | CouponDate | SettlementDate || <br>
+   * ||         1 | 2014-03-30 |     2014-05-02 || <br>
+   * ||         2 | 2014-06-02 |     2014-06-04 || <br>
+   * </pre>
+   *
+   * @param start start date
+   * @param end end date
+   * @param pattern recurrence pattern. Determine how anchor dates will be generated
+   * @param startRule start stub rule, how do we handle fraction period on start? (if any)
+   * @param endRule end stub rule, how do we handle fraction period at the end? (if any)
+   * @param dateDefs a list of date definitions used to generate Date. In this case List(CouponDate, SettlementDate)
+   * @return generated schedule
    *
    * @since 1.0
    */
@@ -45,22 +54,23 @@ object Lamma {
   }
 
   /**
+   * Generate a list of dates from the input criteria
    *
-   * @param start
-   * @param end
-   * @param pattern
-   * @param selector
-   * @param shifter
-   * @param cal
-   * @return
+   * @param start start date
+   * @param end end date
+   * @param pattern recurrence pattern. Determine how next date will be generated from the previous one
+   * @param shifter how to shift the date once it's generated? eg, no shift, 2 days later, 2 working days later
+   * @param selector How to select the date once the date is generated? eg, same day, following business date?
+   * @param cal calendar used to shift and select date
+   * @return a list of generated dates
    *
    * @since 1.0
    */
   def sequence(start: Date,
                end: Date,
                pattern: Recurrence = EveryDay,
-               selector: Selector = SameDay,
                shifter: Shifter = NoShift,
+               selector: Selector = SameDay,
                cal: HolidayCalendar = NoHolidayCalendar) = {
     pattern.gen(start, end).map(shift(_, shifter, cal)).map(select(_, selector, cal))
   }
