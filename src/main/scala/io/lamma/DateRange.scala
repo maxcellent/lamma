23899af5bcd8b51efa758c3828a706611dd0fa97
Max Zhu <hello@zhuchenye.com>
2014-06-10 22:29:05 +0100

    add javaIterable method for DateRange

--- src/main/scala/io/lamma/DateRange.scala
+++ src/main/scala/io/lamma/DateRange.scala
@@ -1,9 +1,15 @@
 package io.lamma
 
 import annotation.tailrec
+import collection.JavaConverters._
 
 case class DateRange(from: Date, to: Date) extends Traversable[Date] {
   override def foreach[U](f: Date => U) = DateRange.eachDate(f, from, to)
+
+  /**
+   * return an instance of java.lang.Iterable can be used in java for comprehension
+   */
+  lazy val javaIterable = this.toIterable.asJava
 }
 
 object DateRange {
---
+++ src/test/java/io/lamma/DateRangeTest.java
@@ -0,0 +1,31 @@
+package io.lamma;
+
+import static io.lamma.LammaConversion.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class DateRangeTest {
+
+    @Test
+    public void testJavaIterator() {
+        List<Date> expected = Lists.newArrayList(date(2014, 5, 5), date(2014, 5, 6), date(2014, 5, 7), date(2014, 5, 8));
+
+        Date fromDate = date(2014, 5, 5);
+        Date toDate = date(2014, 5, 8);
+        DateRange range = fromDate.to(toDate);
+
+        List<Date> actual = new LinkedList();
+        for (Date d: range.javaIterable()) {
+            actual.add(d);
+        }
+
+        assertThat(actual, is(expected));
+    }
+
+}
