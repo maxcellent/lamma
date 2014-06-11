f8f8dd7fa57a1c352e98fd9bd5b0c58538bc49a1
Max Zhu <hello@zhuchenye.com>
2014-06-11 22:38:49 +0100

    add step for DateRange

--- src/main/scala/io/lamma/DateRange.scala
+++ src/main/scala/io/lamma/DateRange.scala
@@ -3,8 +3,37 @@ package io.lamma
 import annotation.tailrec
 import collection.JavaConverters._
 
-case class DateRange(from: Date, to: Date) extends Traversable[Date] {
-  override def foreach[U](f: Date => U) = DateRange.eachDate(f, from, to)
+
+/**
+ * The `DateRange` class represents all date values in range. Both start and end date included. <br>
+ *
+ * This class does not create all Date object to construct a new range. Its complexity is O(1). <br>
+ *
+ * For example:
+ *
+ *  {{{
+ *     Date(2015, 7, 7) to Date(2015, 7, 10) foreach println
+ *  }}}
+ *
+ *  output:
+ *  {{{
+ *    Date(2015,7,7)
+ *    Date(2015,7,8)
+ *    Date(2015,7,9)
+ *    Date(2015,7,10)
+ *  }}}
+ *
+ *  @param from      the start of this range.
+ *  @param to        the exclusive end of the range.
+ *  @param step      the step for the range, default 1
+ *
+ */
+case class DateRange(from: Date, to: Date, step: Int = 1) extends Traversable[Date] {
+  require(step != 0, "step cannot be 0.")
+
+  override def foreach[U](f: Date => U) = DateRange.eachDate(f, from, to, step)
+
+  def by(step: Int) = DateRange(from, to, step)
 
   /**
    * return an instance of java.lang.Iterable can be used in java for comprehension
@@ -14,10 +43,10 @@ case class DateRange(from: Date, to: Date) extends Traversable[Date] {
 
 object DateRange {
   @tailrec
-  def eachDate[U](f: Date => U, current: Date, to: Date): Unit = {
-    if (current <= to) {
+  private def eachDate[U](f: Date => U, current: Date, to: Date, step: Int): Unit = {
+    if ((step > 0 && current <= to) || (step < 0 && current >= to)) {
       f(current)
-      eachDate(f, current + 1, to)
+      eachDate(f, current + step, to, step)
     }
   }
 }
--- src/test/scala/io/lamma/DateRangeSpec.scala
+++ src/test/scala/io/lamma/DateRangeSpec.scala
@@ -1,12 +1,55 @@
 package io.lamma
 
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.{WordSpec, Matchers}
 
-class DateRangeSpec extends FlatSpec with Matchers {
+class DateRangeSpec extends WordSpec with Matchers {
 
-  "DateRange" should "work" in {
-    val expected = Date(2014, 1, 5) :: Date(2014, 1, 6) :: Date(2014, 1, 7) :: Date(2014, 1, 8) :: Nil
-    DateRange(Date(2014, 1, 5), Date(2014, 1, 8)).toList should be(expected)
+  "DateRange" should {
+    "work" in {
+      val expected = Date(2014, 1, 5) :: Date(2014, 1, 6) :: Date(2014, 1, 7) :: Date(2014, 1, 8) :: Nil
+      DateRange(Date(2014, 1, 5), Date(2014, 1, 8)).toList should be(expected)
+    }
+
+    "return itself when from date equals to To date" in {
+      val expected = Date(2014, 5, 1) :: Nil
+      val actual = Date(2014, 5, 1) to Date(2014, 5, 1)
+      actual.toList should be(expected)
+    }
   }
 
+  "by" should {
+    "work for positive step" in {
+      val expected = Date(2014, 1, 5) :: Date(2014, 1, 7) :: Nil
+      val actual = Date(2014, 1, 5) to Date(2014, 1, 8) by 2
+      actual.toList should be(expected)
+    }
+
+    "work for negative step" in {
+      val expected = Date(2014, 1, 8) :: Date(2014, 1, 6) :: Nil
+      val actual = Date(2014, 1, 8) to Date(2014, 1, 5) by -2
+      actual.toList should be(expected)
+    }
+
+    "return itself when from date equals to To date for positive step" in {
+      val expected = Date(2014, 5, 1) :: Nil
+      val actual = Date(2014, 5, 1) to Date(2014, 5, 1) by 2
+      actual.toList should be(expected)
+    }
+
+    "return itself when from date equals to To date for negative step" in {
+      val expected = Date(2014, 5, 1) :: Nil
+      val actual = Date(2014, 5, 1) to Date(2014, 5, 1) by -2
+      actual.toList should be(expected)
+    }
+
+    "return empty list when from date is after To date for positive step" in {
+      val actual = Date(2014, 5, 5) to Date(2014, 5, 1) by 2
+      actual.toList should be(Nil)
+    }
+
+    "return empty list when from date is before To date for negative step" in {
+      val actual = Date(2014, 5, 1) to Date(2014, 5, 5) by -2
+      actual.toList should be(Nil)
+    }
+  }
 }
